34c3bf86ac154307125d8d8882176a0d352f276e
Andy McCall <andymc@smartshuffle.co>
2013-06-23 18:24:17 -0700

    Modified pom.xml for possible to ready for maven release

--- src/main/java/co/smartshuffle/last/fm/Example.java
+++ src/main/java/co/smartshuffle/last/fm/Example.java
@@ -6,11 +6,7 @@ import co.smartshuffle.last.fm.api.ArtistApiClient;
 import co.smartshuffle.last.fm.api.interfaces.ApiKey;
 import co.smartshuffle.last.fm.api.interfaces.ArtistAPI;
 import co.smartshuffle.last.fm.common.DefaultApiKey;
-import co.smartshuffle.last.fm.entities.BaseMusicEntity;
-import co.smartshuffle.last.fm.entities.Tag;
-import co.smartshuffle.last.fm.entities.artist.TopAlbum;
 import co.smartshuffle.last.fm.entities.artist.TopTrack;
-import co.smartshuffle.last.fm.entities.events.Event;
 
 public class Example {
 
